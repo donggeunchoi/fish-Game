d5bac4a5829751567d5fb39859cbf9b25309e904
donggeunchoi <cdg01063149045@gmail.com>
2025-09-11 23:39:23 +0900

    feat : 스테이지씬 해금관련 선언 및 스테이지 선택 기능 구현

--- Assets/Scripts/StageManager/StageManager.cs
+++ Assets/Scripts/StageManager/StageManager.cs
@@ -5,8 +5,40 @@ using UnityEngine.SceneManagement;
 
 public class StageManager : MonoBehaviour
 {
-    public void OnClickInGame()
+    public static StageManager instance { get; private set; }
+    public int SelectedStage { get; private set; } = 1;
+    [SerializeField] private string GameSceneName = "GameScene";
+    [SerializeField] private string stageSelectSceneName = "StageScene";
+  
+    const string KeySelectedStage = "SelectedStage";
+    const string KeyUnlockedMask =  "UnlockedMask";
+
+    public int unlockedMask;
+    
+    void Awake()
     {
-        SceneManager.LoadScene("GameScene");
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        if (PlayerPrefs.HasKey("SelectedStage"))
+        {
+            SelectedStage = PlayerPrefs.GetInt("SelectedStage",1);
+        }
+    }
+
+    public void SelectStage(int stageNum)
+    {
+        SelectedStage = stageNum;
+        PlayerPrefs.SetInt("SelectedStage", stageNum);
+    }
+    public void OnClickInGame(int stageNum)
+    {
+        SelectStage(stageNum);
+        SceneManager.LoadScene(GameSceneName);
     }
 }
