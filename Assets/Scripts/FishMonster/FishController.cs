24956b747d3ba119ae6b1e42b8bcb5799d64c80d
donggeunchoi <cdg01063149045@gmail.com>
2025-09-17 21:10:22 +0900

    fix : 물고기 이동 로직 수정

--- Assets/Scripts/FishMonster/FishController.cs
+++ Assets/Scripts/FishMonster/FishController.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 [RequireComponent(typeof(SpriteRenderer))]
 public class FishController : MonoBehaviour
 {
-
     public FishMonster fishData; // SO 참조
 
     private SpriteRenderer sr;
     private float direction = 1f;
+
+    public int moveRange;
 
     void Awake()
     {
@@ -27,17 +28,15 @@ public class FishController : MonoBehaviour
     {
         if (fishData == null) return;
 
-        // 좌우 이동 예시
-        if (fishData.canSwimLeftRight)
-        {
-            transform.Translate(Vector2.right * fishData.moveSpeed * direction * Time.deltaTime);
+       
+        transform.Translate(Vector2.right * fishData.moveSpeed * direction * Time.deltaTime);
 
-            // 화면 양쪽 끝에서 반전 (예: x 좌표 5 넘어가면 방향 전환)
-            if (Mathf.Abs(transform.position.x) > 5f)
-            {
-                direction *= -1f;
-                sr.flipX = direction < 0f;
-            }
+        // 화면 양쪽 끝에서 반전 (예: x 좌표 5 넘어가면 방향 전환)
+        if (Mathf.Abs(transform.position.x) > 5f)
+        {
+            direction *= -1f;
+            sr.flipX = direction < 0f;
         }
+        
     }
 }
